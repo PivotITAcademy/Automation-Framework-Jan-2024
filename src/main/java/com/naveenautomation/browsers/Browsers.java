1c99418b11fd0632f98e2232ae293d15cfb78571
lijoshabraham <lijoshabraham96@gmail.com>
2024-03-25 20:41:02 -0400

    added android browser

--- src/main/java/com/naveenautomation/browsers/Browsers.java
+++ src/main/java/com/naveenautomation/browsers/Browsers.java
@@ -1,22 +1,22 @@
 package com.naveenautomation.browsers;
 
 public enum Browsers {
-	
-	//This class represent browser
+
+	// This class represent browser
 
 	CHROME("Google Chrome"),
 	EDGE("Microsoft Edge"),
 	FIREFOX("Mozilla Firefox"),
-	SAMSUNG ("Samsung Browser"),
+	SAMSUNG("Samsung Browser"),
 	ANDROID("Android Browser");
-	
+
 	public String browserName;
 
 	Browsers(String browser) {
-		this.browserName=browser;
+		this.browserName = browser;
 	}
 
 	public String getBrowserName() {
 		return browserName;
-	}	
+	}
 }
