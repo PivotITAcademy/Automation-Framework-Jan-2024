761fd8ecef01d5ea443813a070c27442a632c338
RajatGupta2023 <jainrajat0786@gmail.com>
2024-05-04 18:50:18 -0400

    dummy smoke and sanity test added again

--- src/test/java/com/naveenautomation/tests/MyAccountPageTest.java
+++ src/test/java/com/naveenautomation/tests/MyAccountPageTest.java
@@ -21,30 +21,60 @@ public class MyAccountPageTest extends TestBase {
 		intialisation();
 		page = new AccountLoginPage();
 	}
-	
-	@Test(enabled =false)
+
+	@Test(enabled = false)
 	public void validateLogin() {
 		AccountLoginPage page = new AccountLoginPage();
-		myAccountPage=page.submitLogin("mansan@gmail.com", "Password2");
-		String myAccountText=myAccountPage.getMyAccountText();
+		myAccountPage = page.submitLogin("mansan@gmail.com", "Password2");
+		String myAccountText = myAccountPage.getMyAccountText();
 		Assert.assertEquals(myAccountText, "My Munni Account");
 	}
 
-	@Test(enabled=true)
+	@Test(enabled = false)
 	public void validatePasswordUpdate() {
 
-		//Login in
-		myAccountPage=page.submitLogin("mansan@gmail.com", "Password2");
-		//Updating Password
-		changePwdPage=myAccountPage.clickChangePasswordBtn();
-		//Updating Password
-		myAccountPage=changePwdPage.updatePassword("Password2", "Password2");
+		// Login in
+		myAccountPage = page.submitLogin("mansan@gmail.com", "Password2");
+		// Updating Password
+		changePwdPage = myAccountPage.clickChangePasswordBtn();
+		// Updating Password
+		myAccountPage = changePwdPage.updatePassword("Password2", "Password2");
 
 		String pwdAlertMessage = myAccountPage.getPasswordUpdateAlertText();
 
-		//Asserting whether password change is successfully or not
+		// Asserting whether password change is successfully or not
 		Assert.assertEquals("Success: Your password has been successfully updated.", pwdAlertMessage);
 
+	}
+
+	@Test (groups = { "smoke" })
+	public void smokeTest1() {
+		System.out.println("This is smoke test1");
+	}
+	
+	@Test (groups = { "smoke" })
+	public void smokeTest2() {
+		System.out.println("This is smoke test2");
+	}
+	
+	@Test (groups = { "smoke" })
+	public void smokeTest3() {
+		System.out.println("This is smoke test3");
+	}
+	
+	@Test (groups = { "sanity" })
+	public void sanityTest1() {
+		System.out.println("This is sanity test1");
+	}
+	
+	@Test (groups = { "sanity" })
+	public void sanityTest2() {
+		System.out.println("This is sanity test1");
+	}
+	
+	@Test (groups = { "sanity" })
+	public void sanityTest3() {
+		System.out.println("This is sanity test1");
 	}
 
 	@AfterMethod
